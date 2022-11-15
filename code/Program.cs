e46c80af2a6f4743269a6d4100e8f68ae5dbb22f
LucasMiserez <lucas.miserez@student.odisee.be>
2022-11-15 10:38:20 +0100

    setup 2

--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+namespace HocusCodus // Note: actual namespace depends on the project name.
+{
+	internal class Program
+	{
+		static async void Main(string[] args)
+		{
+
+			// Swagger
+			// https://app-htf-2022.azurewebsites.net/swagger/index.html
+			// De httpclient die we gebruiken om http calls te maken
+			var client = new HttpClient();
+
+			// De base url die voor alle calls hetzelfde is
+			client.BaseAddress = new Uri("https://app-htf-2022.azurewebsites.net");
+
+			// De token die je gebruikt om je team te authenticeren, deze kan je via de swagger ophalen met je teamname + password
+			var token = "token";
+
+			// We stellen de token in zodat die wordt meegestuurd bij alle calls, anders krijgen we een 401 Unauthorized response op onze calls
+			client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+			// De url om de challenge te starten
+			var startUrl = "api/path/1/easy/Start";
+
+			// We voeren de call uit en wachten op de response
+			// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/await
+			// De start endpoint moet je 1 keer oproepen voordat je aan een challenge kan beginnen
+			// Krijg je een 403 Forbidden response op je Sample of Puzzle calls? Dat betekent dat de challenge niet gestart is
+			var startResponse = await client.GetAsync(startUrl);
+
+			// De url om de sample challenge data op te halen
+			var sampleUrl = "api/path/1/easy/Sample";
+
+			// We doen de GET request en wachten op de het antwoord
+			// De response die we verwachten is een lijst van getallen dus gebruiken we List<int>
+			var sampleGetResponse = await client.GetFromJsonAsync<List<int>>(sampleUrl);
+
+			
+		}
+	}
+}
